1ed43a30c74c8ff74ccacc14437b1819c0621c15
Huy Truong Le <truonglehuy2007@gmail.com>
2021-08-01 12:25:18 +0700

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,27 @@
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            switch (event.type)
+            {
+            case sf::Event::Closed:
+                window.close();
+            }
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
+}
